475c62df5cdff1d33c3e60caa6deebccedde16b5
Paul Timmins <paul@iqmo.com>
2023-04-22 11:30:41 -0400

    Updated for Cell Magics and simplifying

--- jupylite_duckdb/jdw.py
+++ jupylite_duckdb/jdw.py
@@ -1,58 +1,83 @@
 import js
 from pandas import DataFrame
 from typing import Optional
+from js import globalThis
 
-async def query(sql: str, connection: object = None) -> DataFrame:
-    """Executes query in a standalone connection"""
-    if connection is not None:
-        result_promise = connection.query(sql)
-    else:
-        js_function = js.Function('obj', '''
-            async function executeSqlDuckdb() {
-                    let c = undefined
-                    if(obj.connection == undefined) {
-                        const duckdb = await import('https://cdn.skypack.dev/@duckdb/duckdb-wasm');
-                        const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
-                        const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+# TODO Laundry List
+# - Pass arrow from duckdb_wasm to Python efficiently, altho dependent on pyodide pyarrow?
+# - Register pandas dataframes with duckdb_wasm: maybe use get_table_names to determine
+# what needs to be registered, then register
+# - Update connect to take connect(file)
+# - 
 
-                        const worker_url = URL.createObjectURL(
-                            new Blob([`importScripts("${bundle.mainWorker}");`], { type: 'text/javascript' })
-                        );
-                        const worker = new Worker(worker_url);
-                        const logger = new duckdb.ConsoleLogger();
-                        const db = new duckdb.AsyncDuckDB(logger, worker);
-                        await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
-                        c = await db.connect();
-                    }
-                    else {
-                        c = obj.connection
-                    }
 
-                    console.log('Running SQL: ', obj.sql)
-                    const sql = obj.sql;
-                    const result = await c.query(sql);
-
-                    console.log('Result:', result);
-                    return result
-                }
-            return executeSqlDuckdb()
-        ''')
-
-        js_obj = js.Object() 
-        js_obj.sql = sql
-        js_obj.connection = connection
-
-        result_promise = js_function(js_obj)    # <class 'pyodide.webloop.PyodideFuture'>
-    
-    obj = await result_promise
+async def future_to_df(result_promise):
+    obj = await result_promise # <class 'pyodide.ffi.JsProxy'>
     a = obj.toArray()
-    data = [dict(v) for v in a.object_values()]
+    data = [dict(v) for v in a.object_values()] 
 
     df = DataFrame(data)
-    return df  # <class 'pyodide.ffi.JsProxy'>
+    return df
+
+async def query(sql: str, connection: object = None, return_future= False) -> DataFrame:
+    """Executes query in a standalone connection"""
+    try:
+        if connection is not None:
+            result_promise = connection.query(sql)
+        else:
+            js_function = js.Function('obj', '''
+                async function executeSqlDuckdb() {
+                        let c = undefined
+                        if(obj.connection == undefined) {
+                            const duckdb = await import('https://cdn.skypack.dev/@duckdb/duckdb-wasm');
+                            const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
+                            const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+
+                            const worker_url = URL.createObjectURL(
+                                new Blob([`importScripts("${bundle.mainWorker}");`], { type: 'text/javascript' })
+                            );
+                            const worker = new Worker(worker_url);
+                            const logger = new duckdb.ConsoleLogger();
+                            const db = new duckdb.AsyncDuckDB(logger, worker);
+                            await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+                            c = await db.connect();
+                        }
+                        else {
+                            c = obj.connection
+                        }
+
+                        console.log('Running SQL: ', obj.sql)
+                        const sql = obj.sql;
+                        const result = await c.query(sql);
+
+                        console.log('Result:', result);
+                        return result
+                    }
+                return executeSqlDuckdb()
+            ''')
+            js_obj = js.Object() 
+            js_obj.sql = sql
+            js_obj.connection = connection
+
+            result_fut = js_function(js_obj)    # <class 'pyodide.webloop.PyodideFuture'>
+        
+        if return_future: 
+            return result_fut
+        else:
+            return await future_to_df(result_fut)
+    except Exception as e:
+        print(e)
+        return None
     
 
 async def connect() -> object:
+    # Other approaches:
+    # Store the function in globalThis, instead of creating a new function
+    # on connect
+    # then access via import GlobalThis; globalThis.connectDuckDb()
+    #
+    # //  globalThis.connectDuckDb = connectDuckDb;
+
     js_function = js.Function('obj', '''
         async function connectDuckdb() {
                 console.log("Connecting to " + obj.connectionstr);
@@ -85,3 +110,118 @@ async def connect() -> object:
     connection = await result_promise
 
     return connection
+
+def register_iiafes():
+    syncWrapperConnect_js = js.Function('obj', '''
+        function syncWrapperConnect(obj) {
+            //delete globalThis.connection;
+            (async () => {
+                async function executeSqlDuckdb() {
+                        console.log("Connecting to " + obj.connectionstr);
+                        const duckdb = await import('https://cdn.skypack.dev/@duckdb/duckdb-wasm');
+                        const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
+                        const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+
+                        const worker_url = URL.createObjectURL(
+                            new Blob([`importScripts("${bundle.mainWorker}");`], { type: 'text/javascript' })
+                        );
+                        const worker = new Worker(worker_url);
+                        const logger = new duckdb.ConsoleLogger();
+                        const db = new duckdb.AsyncDuckDB(logger, worker);
+                        await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+                        const c = await db.connect(obj.connectionstr);
+
+                        console.log('Result:', c);
+
+                        globalThis.connection=result;
+                        return result;
+                    }
+                try {
+      const result = await executeSqlDuckdb();
+      // Process the result
+      console.log('Result:', result);
+      
+    } catch (error) {
+      // Handle errors
+      console.error('Error:', error);
+    }
+                }
+                
+            )();
+        }
+        globalThis.syncWrapperConnect=syncWrapperConnect
+    ''')
+
+    syncWrapperConnect_js() # function stored on globalThis.syncWrapperConnect
+
+
+    
+    syncWrapper_query = js.Function('obj', '''
+        function syncWrapperQuery(obj) {
+            globalThis.result=null;
+            (async () => {
+                async function executeSqlDuckdb() {
+                        //delete globalThis.result;
+                        if(obj.connection == undefined) {
+                            const duckdb = await import('https://cdn.skypack.dev/@duckdb/duckdb-wasm');
+                            const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
+                            const bundle = await duckdb.selectBundle(JSDELIVR_BUNDLES);
+
+                            const worker_url = URL.createObjectURL(
+                                new Blob([`importScripts("${bundle.mainWorker}");`], { type: 'text/javascript' })
+                            );
+                            const worker = new Worker(worker_url);
+                            const logger = new duckdb.ConsoleLogger();
+                            const db = new duckdb.AsyncDuckDB(logger, worker);
+                            await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
+                            c = await db.connect();
+                        }
+                        else {
+                            c = obj.connection
+                        }
+
+                        console.log('Running SQL: ', obj.sql)
+                        const sql = obj.sql;
+                        const result = await c.query(sql);
+
+                        console.log('Result:', result);
+                        globalThis.objresult=result;
+                        return result;
+                    }
+                try {
+      const result = await executeSqlDuckdb();
+      // Process the result
+      console.log('Result:', result);
+      
+    } catch (error) {
+      // Handle errors
+      console.error('Error:', error);
+    }
+                }
+                
+            )();
+        }
+        globalThis.syncWrapperQuery=syncWrapperQuery
+    ''')
+    syncWrapper_query() # function stored on globalThis.syncWrapperQuery
+
+def connect_sync(connstr: str = ":memory:"):
+
+    js_obj_conn = js.Object()
+    js_obj_conn.connectionstr = connstr
+    globalThis.syncWrapperConnect(js_obj_conn)
+
+
+def query_sync(sql: str) -> DataFrame:
+        
+    js_obj_q = js.Object()
+    js_obj_q.connection = globalThis.connection
+    js_obj_q.sql=sql
+
+    globalThis.syncWrapperQuery(js_obj_q)
+    
+    a = globalThis.objresult.toArray()
+    data = [dict(v) for v in a.object_values()]
+    df = DataFrame(data)
+    
+    return df
---
+++ jupylite_duckdb/jdw_magic.py
@@ -0,0 +1,35 @@
+# Inspired by: https://github.com/jupyterlite/jupyterlite/issues/237
+
+from IPython.core.magic import register_line_magic, register_cell_magic
+from IPython.core import magic_arguments
+from IPython import display
+import ipywidgets as widgets
+import asyncio
+import jupylite_duckdb as jd
+import functools
+
+
+async def display_result(obj, output):
+        with output:
+            display(obj)
+            
+@register_line_magic
+@register_cell_magic
+@magic_arguments.magic_arguments()
+@magic_arguments.argument('query', nargs="+", help="Query.", type=str)
+def dql(line = "", cell = ""):
+    global connection
+    if line:
+        args = magic_arguments.parse_argstring(dql, line)
+        sql = ' '.join(args.query)
+
+    elif cell:
+        sql= cell
+
+
+    s_out = widgets.Output(layout={'border': '1px solid black'})
+    display(s_out)
+    with s_out:
+        r = asyncio.get_event_loop().run_until_complete(jd.query(sql=sql, return_future=False))
+        r.then(functools.partial(display_result, output=s_out))
+
