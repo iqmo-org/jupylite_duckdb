512b0670f05bc7890f8be02720cfcf3fba54eb65
Paul Timmins <paul@iqmo.com>
2023-04-22 20:18:52 -0400

    Patching transform_cell to rewrite to an await

--- jupylite_duckdb/jdw_magic.py
+++ jupylite_duckdb/jdw_magic.py
@@ -1,10 +1,16 @@
+# Still a work in progress:
+# The complexity here is everything is async and cell magics aren't. 
+# %%autoawait gave me some ideas. The workaround right now is to 
+# monkey patch ipython.transform_cell to rewrite the %%dql
+# cell to:
+# obj_df = await jd.query(sql=query) 
+# 
+
 from IPython.core.magic import register_line_magic, register_cell_magic
 from IPython.core import magic_arguments
 from IPython.display import display
 import ipywidgets as widgets
-import asyncio
 import jupylite_duckdb as jd
-import functools
 from IPython.core.getipython import get_ipython
 
 DEBUG = True
@@ -39,27 +45,39 @@ def dql(line = "", cell = ""):
     s_out = widgets.Output(layout={'border': '1px solid black'})
     display(s_out)
 
-    with s_out:
-            r = asyncio.get_event_loop().run_until_complete(jd.query(sql=sql, return_future=False))
-            r.then(functools.partial(display_result, outputvar = outputvar, output=s_out))
+    df = jd.query(sql=sql, return_future=False)
+    if outputvar is not None: 
+        get_ipython().user_ns[outputvar] = df  # type: ignore
+    # with s_out:
+    #        r = asyncio.get_event_loop().run_until_complete(jd.query(sql=sql, return_future=False))
+    #        r.then(functools.partial(display_result, outputvar = outputvar, output=s_out))
 
 
 def patch_magic():
     # Monkey patching a transformation to stick an "await" ahead of this cell magic
-    
-    
     shell = get_ipython()
 
     if not hasattr(shell, "_orig_transform_cell"):
         shell._orig_transform_cell = shell.transform_cell
 
-    def transform_cell(*args, **kwargs) -> bool:
-            #print(args)
-            #print(kwargs)
-            
-            result=shell._orig_transform_cell(*args, **kwargs)
-            if result.startswith("get_ipython().run_cell_magic('dql',"):
-                result="await" + result
+    def jd_transform_cell(*args, **kwargs) -> bool:
+        orig_cell = args[0]
+        if orig_cell.startswith("%%dql"):
+            lines = orig_cell.split("\n")
+            first_line=lines[0]
+            first_line=first_line.replace("%%dql", "").strip()
+            rest = "\\n".join(lines[1:])
+            rest=rest.replace("'", "\\'")
+            if len(first_line) >0:
+                first_line = f"{first_line} = "
+            result= f"{first_line}await jd.query(sql='{rest}')"
+            print(result)
             return result
+        
+        result=shell._orig_transform_cell(*args, **kwargs)
+        return result
+    
+    shell.transform_cell = jd_transform_cell
     
 patch_magic()
+# %%
